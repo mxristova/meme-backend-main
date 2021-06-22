62c33229081f7dfcb9a829fe7c3e5eec83c5171f
mxristova <79445531+mxristova@users.noreply.github.com>
2021-06-22 10:52:45 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0f975a47de4000197797736b4d0bb575'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cb6c647c-8c36-4f0e-b60d-8440a4d13383'
+};
